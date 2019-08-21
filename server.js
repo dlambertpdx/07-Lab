6f705066e9792b110d219ac34021c71a9349e69f
sanvean74 <dlambertpdx@gmail.com>
2019-08-21 09:44:59 -0700

    connected maps api to server

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ require ('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const mapsApi = require('./lib/maps-api');
+
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 
 app.get('/location', (request, response) => {
-    try {
-        const location = request.query.location;
-        const result = getLatLng(location);
-        response.status(200).json(result);
-    }
-    catch(err) {
-        response.status(500).send('Sorry, something went wrong :(');
-    }
+    const search = request.query.search;
+    mapsApi.getLocation(search)
+        .then(location => {
+            response.json(location);
+        })
+        
+        .catch(err => {
+            response.status(500).json({
+                error: err.message || err
+            });
+
+        });
+    
 });
 
 app.get('/weather', (request, response) => {
@@ -41,7 +48,7 @@ function toDay() {
     return {
         forecast: forecast,
         time: time
-    }
+    };
 }
 
 
