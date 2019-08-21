7f74d6a0cd434a81f99023411550a8f1100acac8
sanvean74 <dlambertpdx@gmail.com>
2019-08-21 09:37:28 -0700

    refactored map api

---
+++ server.js
@@ -0,0 +1,52 @@
+require ('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+
+app.get('/location', (request, response) => {
+    try {
+        const location = request.query.location;
+        const result = getLatLng(location);
+        response.status(200).json(result);
+    }
+    catch(err) {
+        response.status(500).send('Sorry, something went wrong :(');
+    }
+});
+
+app.get('/weather', (request, response) => {
+    try {
+        const weather = request.query.weather;
+        const result = getWeather(weather);
+        response.status(200).json(result);
+    }
+    catch(err) {
+        response.status(500).send('Sorry, something went wrong :(');
+    }
+});
+
+const forecastData = require('./data/darksky.json');
+function getWeather() {
+    return toDay(forecastData);
+}
+
+function toDay() {
+    const time = forecastData.daily.data[0].time;
+    const forecast = forecastData.daily.data[0].summary;
+
+    return {
+        forecast: forecast,
+        time: time
+    }
+}
+
+
+
+
+app.listen(PORT, () => {
+    console.log('server is running on PORT', PORT);
+});
